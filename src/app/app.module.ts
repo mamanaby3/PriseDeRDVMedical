89377422e2b2d63dcb539505caed78beab48bacd
Mame Aby <mameabydrame@gmail.com>
2024-08-14 15:34:42 +0000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
